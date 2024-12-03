3c50f4f344fd572c7fd1f9bbedcb450a0541f6d6
PreamPSss <pattapon.sael@bumail.net>
2024-12-03 16:27:40 +0700

    final

--- Assets/Exam1/Bird.cs
+++ Assets/Exam1/Bird.cs
@@ -2,15 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public  class Bird : Animal
+public class Bird : Animal
 {
-    Init (string ainmalName)
-    { 
-        ainmalName = "Bird" ;
-    }
-
-    Makesound() { }
-
+    float dailyFoodConsumption = 0f;
+    int activityLeave = 0; 
 
 
 }
